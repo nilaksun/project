968f53715219a57a45027a0533bb40544418fc17
nilaksun <133011755+nilaksun@users.noreply.github.com>
2023-07-11 15:21:58 +0300

    Add files via upload

---
+++ project.py
@@ -0,0 +1,36 @@
+import nltk
+from nltk.tokenize import sent_tokenize,word_tokenize
+import os
+import argparse
+
+sentence_list = []
+word_list = []
+
+parser = argparse.ArgumentParser(description='Read a text file and word list file and find sentences')
+parser.add_argument('--book_name', required=True, help='Name of the book to read')
+parser.add_argument('--word_list', required=True, help='Name of the word list to use')
+
+args = parser.parse_args()
+
+
+book_name = args.book_name 
+word_list = args.word_list
+
+with open(args.book_name, 'r', encoding="utf-8") as f:
+    book_content = f.read()
+
+    for sentence in sent_tokenize(book_content):
+        sentence_list.append(sentence)
+
+with open(args.word_list, 'r', encoding="utf-8") as f:
+    vocab_content = f.read()
+
+    for words in word_tokenize(vocab_content):
+        word_list.append(words)
+
+def check(sentence, words):
+	res = [any([k in s for k in words]) for s in sentence]
+	return [sentence[i] for i in range(len(res)) if res[i]]
+
+
+print(check(sentence=sentence_list, words=word_list))
